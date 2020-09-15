260a53c8daff7c8bdd4b7ab0f06004006ea80576
JnDiego <diego_suarez@outlook.com>
2020-09-14 21:29:37 -0500

    Updates

---
+++ precache-manifest.375e987dda0d0cd6a785108a5e0bccb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2722ccf57ca7170431fb2f4ed3eedb",
+    "url": "/drink-recipes/index.html"
+  },
+  {
+    "revision": "59f605d1b384bf5a3d45",
+    "url": "/drink-recipes/static/css/main.722f8f3a.chunk.css"
+  },
+  {
+    "revision": "af6f6c91996709cf02cd",
+    "url": "/drink-recipes/static/js/2.0eb12888.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/drink-recipes/static/js/2.0eb12888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f605d1b384bf5a3d45",
+    "url": "/drink-recipes/static/js/main.92c7f51a.chunk.js"
+  },
+  {
+    "revision": "d47135f8870bcead00f3",
+    "url": "/drink-recipes/static/js/runtime-main.9fcb9845.js"
+  }
+]);
